247e0881a216af15b96e304f31dd45be1cafae1c
Chaimite <adrianocorreiacesar@gmail.com>
2019-06-10 14:16:45 +0100

    fixe functionality in Card class and almost done with Deck class

--- test/testMain.java
+++ test/testMain.java
@@ -2,8 +2,11 @@ package test;
 
 import java.nio.file.FileSystems;
 import java.nio.file.Path;
+import java.util.ArrayList;
 
 import util.FileOutput;
+import model.Card;
+import model.Deck;
 
 
 public class testMain
@@ -13,6 +16,8 @@ public class testMain
    {
       
       testReader();
+      testCard();
+      testDeck();
    }
 
    public static void testReader() {
@@ -21,5 +26,26 @@ public class testMain
       
       FileOutput.getCards(filePath);
       FileOutput.getFieldNames(filePath);
+   }
+   public static void testCard() {
+      Card card = new Card("Rocky", "10", "8", "6", "7", "5");
+      System.out.println("Show the rocky card");
+      System.out.println(card.toString());
+      
+   }
+   public static void testDeck() {
+      Card card1 = new Card("Rocky", "10", "8", "6", "7", "5");
+      Card card2 = new Card("Cluber Lang", "8", "9", "5", "9", "10");
+      Card card3 = new Card("Appollo Creed", "9", "8", "5", "10", "5");
+      Card card4 = new Card("Ivan Drago", "10", "10", "10", "10", "5");
+      ArrayList<Card> deck = new ArrayList<Card>();
+      deck.add(card1);
+      deck.add(card2);
+      deck.add(card3);
+      deck.add(card4);
+      System.out.println(deck);
+      Deck.getShuffledDeck(deck);
+      System.out.println(deck);
+      
    }
 }
